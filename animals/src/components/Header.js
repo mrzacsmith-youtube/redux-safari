a4354228a083427cad6164fc17a0262c1b40f3ce
Zac Smith <zrs3141592@gmail.com>
2020-10-15 19:49:25 -0600

    update header and login

--- animals/src/components/Header.js
+++ animals/src/components/Header.js
@@ -1,20 +1,27 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
+  const signOut = () => {
+    window.localStorage.removeItem('token')
+  }
 
-    const signOut = ( ) => {
-        window.localStorage.removeItem('token');
-    }
-
-    return (
-        <div className="header">
-            <Link to="/" className="title">Safari App</Link>
-            <nav className="nav-links">
-                <Link className="nav-link" to ="/login">Login</Link>
-                <Link className="nav-link" to ="/creatures">Animals</Link>
-                <Link className="nav-link" to ="/login" onClick={signOut}>Sign out</Link>
-            </nav>
-        </div>
-    )
+  return (
+    <div className='header'>
+      <Link to='/' className='title'>
+        Safari App
+      </Link>
+      <nav className='nav-links'>
+        <Link className='nav-link' to='/login'>
+          Login
+        </Link>
+        <Link className='nav-link' to='/creatures'>
+          Animals
+        </Link>
+        <Link className='nav-link' to='/login' onClick={signOut}>
+          Sign out
+        </Link>
+      </nav>
+    </div>
+  )
 }
